be5cb1f1333a0949ea5b5d212534312c6af052b8
Prashant Singh Pawar <ppawar@inrhythm.com>
2013-04-04 15:01:15 -0400

    added more methods for the service

--- app/js/services/roadmapService.js
+++ app/js/services/roadmapService.js
@@ -31,6 +31,36 @@ define(['services/utilService'], function (utilService) {
             addRoadmap: function (roadmap) {
                 roadmaps.push(roadmaps);
                 utilService.saveToLocal(key, roadmaps);
+            },
+            deleteRoadmap: function (roadmaps, roadmapIndex) {
+                roadmaps.splice(roadmapIndex, 1);
+                utilService.saveToLocal(key, roadmaps);
+            },
+            addTheme: function (roadmap, theme) {
+                roadmap.themes.push(theme);
+                utilService.saveToLocal(key, roadmaps);
+            },
+            deleteTheme: function (roadmap, themeIndex) {
+                roadmap.themes.splice(themeIndex, 1);
+                utilService.saveToLocal(key, roadmaps);
+            },
+            addStory: function (theme, story) {
+                //Make sure that the stories are displayed in chronological
+                //order using Angular Filter
+                theme.stories.push(story);
+                utilService.saveToLocal(key, roadmaps);
+            },
+            deleteStory: function (theme, storyIndex) {
+                theme.stories.splice(storyIndex, 1);
+                utilService.saveToLocal(key, roadmaps);
+            },
+            addDetail: function (story, detail) {
+                story.details.push(detail);
+                utilService.saveToLocal(key, roadmaps);
+            },
+            deleteDetail: function (story, detailIndex) {
+                story.details.splice(detailIndex, 1);
+                utilService.saveToLocal(key, roadmaps);
             }
         };
     }]];
