d3c02b0f4ae4bb63284fe1843ca4401bdbb0ac8c
szhanginrhythm <szhang@inrhythm.com>
2013-04-04 14:47:59 -0400

    new update

--- app/js/services/roadmapService.js
+++ app/js/services/roadmapService.js
@@ -3,6 +3,24 @@
 define(['services/utilService'], function (utilService) {
     return ['roadmapService', ['utilService', function (utilService) {
 
+//        roadmap {
+//            name: '',
+//            type: 'Monthly' or 'Quarterly',
+//            startTime: 'M/Y'
+//            themes: [
+//                      {
+//                          name:
+//                          stories: [
+//                                      {
+  //                                     time:
+//                                       details: [d1, d2, d3]
+//                                      }
+
+//                                      ]
+//                      }
+//                    ]
+//        }
+
         var key = 'roadmaps',
             roadmaps = utilService.getFromLocal(key);
 
