aefa4f5798e94d26303b0f223f5722078fce392f
szhanginrhythm <szhang@inrhythm.com>
2013-04-04 13:49:30 -0400

    updates

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,4 @@ define(['angular', 'services'], function (angular, services) {
 	'use strict';
 	
 	angular.module('productRoadmap.filters', ['productRoadmap.services']);
-//		.filter('interpolate', ['version', function(version) {
-//			return function(text) {
-//				return String(text).replace(/\%VERSION\%/mg, version);
-//			};
-//	}]);
 });
